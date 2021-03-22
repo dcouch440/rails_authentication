e23ca4a462effdda3db895f5d2f5b4e8d4f00f7a
dcouch440 <dcouch440@gmail.com>
2021-03-22 11:43:00 -0700

    create project, add user login
    Co-authored-by: joanna-miller <joanna-miller@users.noreply.github.com>

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root 'homes#index'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
+
+end
